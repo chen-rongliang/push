f0f88ae7ecabbf2aba888a8aa1b935a77bdd1ffd
chen-rongliang@qq.com <Dagou520**>
2020-10-13 14:52:14 +0800

    fixed task

--- task/steps/index.js
+++ task/steps/index.js
@@ -7,6 +7,7 @@ const list = require('./list.json')
 const url = 'http://api.eyoutw.com/fed/test/steps/submit.php'
 
 list.forEach(data => {
+    // 遍历json数据后发出请求
     data.step = Utils.random(1e4, 2e4)
     fetch(`${url}?${qs.stringify(data)}`)
 })
