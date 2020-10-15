13bd995272d6b93d0a55243ce8eb009cf32ab042
chen-rongliang@qq.com <Dagou520**>
2020-10-15 15:54:56 +0800

    任务调整，限制入口文件

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const Addressing = require('./lib/addressing')
 
 // 遍历可执行文件后，开始运行任务
-Addressing('task', 'js').forEach(entry => {
-    require(`./${entry}`)
+let task = Addressing('task', 'js')
+Object.keys(task).forEach(name => {
+    // 限制一下，只运行目录下的entry.js
+    if(/entry\.js/.test(name)) {
+        require(`./${task[name]}`)
+    }
 })
