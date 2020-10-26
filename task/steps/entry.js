1d2c64c38405976efd5bd6a1d8217a6546c0de9e
chen-rongliang@qq.com <Dagou520**>
2020-10-26 10:02:08 +0800

    尝试另一种方式请求

--- task/steps/entry.js
+++ task/steps/entry.js
@@ -13,69 +13,76 @@ const Utils = require('../../lib/utils')
 const API = require('./api.js')
 const list = require('./list.json')
 
-// post压缩
-function post (url, data, headers) {
-    return fetch(url, {
-        method: 'post',
-        body: JSON.stringify(data),
-        headers: Object.assign({
-            'Content-Type': 'application/json; charset=utf-8'
-        }, headers)
-    }).then(res => res.json())
-}
+console.log('开始刷步任务')
 
-list.forEach(async ({ loginName, password }) => {
-    // 遍历账号信息，开始登录
-    const {code, data} = await post(API.login, {
-        appType: 6,
-        clientId: "88888",
-        loginName,
-        password,
-        roleType: 0
-    })
+~(async function () {
 
-    // 登录成功
-    if(code == 200) {
-        console.log(`<${loginName}>登录成功!`)
-
-        // 登录后参数提取
-        const { accessToken, userId } = data
-        // 步数模拟，可设置为固定值，或者像这样的区间值随机数
-        const step = Utils.random(1e4, 2e4)
-        // 时间
-        const time = new Date()
-
-        // 开始刷步数
-        post(API.submit, {
-            list: [
-                {
-                    active: 1,
-                    calories: Math.floor(step/4),
-                    DataSource: 2,
-                    dataSource: 2,
-                    deviceId: "M_NULL",
-                    distance: Math.floor(step/3),
-                    exerciseTime: 0,
-                    isUpload: 0,
-                    measurementTime: Utils.timeFormat(time, 'yyyy-mm-dd hh:MM:ss'),
-                    priority: 0,
-                    step,
-                    type: 2,
-                    updated: time / 1,
-                    userId
-                }
-            ]
-        }, {
-            Cookie: `accessToken=${accessToken}`
-        }).then(({ code }) => {
-            if(code == 200) {
-                console.log(`<${loginName}>刷步成功!`)
-            } else {
-                console.error(`<${loginName}>刷步失败[code:${code}]`)
-            }
-        })
-
-    } else {
-        console.error(`登录失败[code:${code}]`)
+    // post压缩
+    function post (url, data, headers) {
+        return fetch(url, {
+            method: 'post',
+            body: JSON.stringify(data),
+            headers: Object.assign({
+                'Content-Type': 'application/json; charset=utf-8'
+            }, headers)
+        }).then(res => res.json())
     }
-})
+
+    for(let { loginName, password } of list) {
+        // 遍历账号信息，开始登录
+        let { code, data } = await post(API.login, {
+            appType: 6,
+            clientId: "88888",
+            loginName,
+            password,
+            roleType: 0
+        })
+            
+        // 登录成功
+        if(code == 200) {
+            console.log(`<${loginName}>登录成功!`)
+
+            // 登录后参数提取
+            const { accessToken, userId } = data
+            // 步数模拟，可设置为固定值，或者像这样的区间值随机数
+            const step = Utils.random(1e4, 2e4)
+            // 时间
+            const time = new Date()
+            // 请求参数
+            const param = {
+                active: 1,
+                DataSource: 2,
+                dataSource: 2,
+                deviceId: "M_NULL",
+                exerciseTime: 0,
+                isUpload: 0,
+                priority: 0,
+                type: 2,
+                calories: Math.floor(step/4),
+                distance: Math.floor(step/3),
+                measurementTime: Utils.timeFormat(time, 'yyyy-mm-dd hh:MM:ss'),
+                step,
+                updated: time / 1,
+                userId
+            }
+
+            // 刷！
+            let result = await post(API.submit, {list: [ param ]},{
+                Cookie: `accessToken=${accessToken}`
+            })
+
+            if(result.code == 200) {
+                console.log(`<${loginName}>刷步成功! 当前步数：${step}。`)
+            } else {
+                console.error(`<${loginName}>刷步失败[code:${result.code}]`)
+            }
+
+        } else {
+            console.error(`登录失败[code:${code}]`)
+        }
+
+    }
+
+    console.log('刷步结束')
+
+})()
